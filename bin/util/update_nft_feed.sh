fa2cf51a71889ebf16a2e213fe30de473b3a63a8
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2020-05-02 16:48:48 +0200

    added risk to feed

--- bin/util/update_nft_feed.sh
+++ bin/util/update_nft_feed.sh
@@ -1,43 +1,56 @@
 #! /usr/bin/env bash
 
-
-
-CACHE_DIRECTORY="/etc/cache"
-
-# Loop through arguments and process them
+# fetch arguments
 for arg in "$@"
 do
     case $arg in
         -r=*|--registry=*)
         NFT_REGISTRY="${arg#*=}"
-        shift # Remove --cache= from processing
+        shift
         ;;
         -t=*|--tokenId=*)
         TOKEN_ID="${arg#*=}"
-        shift # Remove --cache= from processing
+        shift
         ;;
         -v=*|--value=*)
         NFT_VALUE="${arg#*=}"
-        shift # Remove --cache= from processing
+        shift
+        ;;
+        -r=*|--risk=*)
+        RISK="${arg#*=}"
+        shift
         ;;
     esac
 done
 
 BIN_DIR=${BIN_DIR:-$(cd "${0%/*}"&&pwd)}
 source $BIN_DIR/util.sh
-message Update NFT Feed
 
 cd $BIN_DIR
 ADDRESSES="./../../deployments/addresses_$(seth chain).json"
 
 loadValuesFromFile $ADDRESSES
-
+message Input Data
 echo "NFT Registry Address: $NFT_REGISTRY"
 echo "Token id: $TOKEN_ID"
 echo "NFT Value: $NFT_VALUE"
-NFT_ID=$(seth call $NFT_FEED 'nftID(address,uint)' $NFT_REGISTRY $TOKEN_ID)
+echo "Risk Group: $RISK"
 
-seth send $NFT_FEED 'update(bytes32, uint)' $NFT_ID $NFT_VALUE
+NFT_ID=$(seth call $NFT_FEED 'nftID(address,uint)' $NFT_REGISTRY $TOKEN_ID)
+if [ ! -z "$RISK" ]
+then
+     message Update Feed with Value and Risk
+     seth send $NFT_FEED 'update(bytes32, uint, uint)' $NFT_ID $NFT_VALUE $RISK
+else
+    message Update Feed with Value
+     seth send $NFT_FEED 'update(bytes32, uint)' $NFT_ID $NFT_VALUE
+fi
+
+
+
+message Call Contract to verify
+echo "NFT Value: $(seth call $NFT_FEED 'nftValues(bytes32)(uint)' $NFT_ID)"
+echo "Risk Group: $(seth call $NFT_FEED 'risk(bytes32)(uint)' $NFT_ID)"
 
 
 
