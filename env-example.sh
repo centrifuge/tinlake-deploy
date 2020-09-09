83015fd79b5211bc65493e785bf5ef39ac20d49d
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2020-09-09 18:58:24 +0200

    Add example env file

---
+++ env-example.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+export ETH_RPC_URL=http://127.0.0.1:8545
+export ETH_GAS_PRICE=7000000
+export ETH_KEYSTORE=~/.ethsign/tinlake_deployment/kovan
+export ETH_PASSWORD=~/.ethsign/tinlake_deployment/kovan/passphrase
