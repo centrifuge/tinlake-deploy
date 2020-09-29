1e56bc47b6384f81c89014447b8d534015cb19ce
Charly <charly@centrifuge.io>
2020-09-29 12:41:59 +0200

    update lender deploy to verify contracts

--- bin/lender/deploy.sh
+++ bin/lender/deploy.sh
@@ -4,7 +4,8 @@ set -e
 
 BIN_DIR=${BIN_DIR:-$(cd "${0%/*}"&&pwd)}
 source $BIN_DIR/util/util.sh
-CONTRACT_BIN=$BIN_DIR/../lib/tinlake/out
+export DAPP_JSON=$BIN_DIR/../lib/tinlake/out/dapp.sol.json
+export DAPP_ROOT=$BIN_DIR/../lib/tinlake
 
 cd $BIN_DIR
 
@@ -15,25 +16,25 @@ ZERO_ADDRESS=0x0000000000000000000000000000000000000000
 
 message Fetch Fab Addresses or Deploy
 
-RESERVE_FAB=$(getFabContract $CONTRACT_BIN/ReserveFab.bin "RESERVE_FAB")
+RESERVE_FAB=$(getFabContract src/lender/fabs/reserve.sol:ReserveFab "RESERVE_FAB")
 message "RESERVE_FAB: $RESERVE_FAB"
 
-ASSESSOR_FAB=$(getFabContract $CONTRACT_BIN/AssessorFab.bin "ASSESSOR_FAB")
+ASSESSOR_FAB=$(getFabContract src/lender/fabs/assessor.sol:AssessorFab "ASSESSOR_FAB")
 message "ASSESSOR_FAB: $ASSESSOR_FAB"
 
-TRANCHE_FAB=$(getFabContract $CONTRACT_BIN/TrancheFab.bin "TRANCHE_FAB")
+TRANCHE_FAB=$(getFabContract  src/lender/fabs/tranche.sol:TrancheFab "TRANCHE_FAB")
 message "TRANCHE_FAB: $TRANCHE_FAB"
 
-MEMBERLIST_FAB=$(getFabContract $CONTRACT_BIN/MemberlistFab.bin "MEMBERLIST_FAB")
+MEMBERLIST_FAB=$(getFabContract src/lender/fabs/memberlist.sol:MemberlistFab "MEMBERLIST_FAB")
 message "MEMBERLIST_FAB: $MEMBERLIST_FAB"
 
-RESTRICTEDTOKEN_FAB=$(getFabContract $CONTRACT_BIN/RestrictedTokenFab.bin "RESTRICTEDTOKEN_FAB")
+RESTRICTEDTOKEN_FAB=$(getFabContract src/lender/fabs/restrictedtoken.sol:RestrictedTokenFab "RESTRICTEDTOKEN_FAB")
 message "RESTRICTEDTOKEN_FAB: $RESTRICTEDTOKEN_FAB"
 
-OPERATOR_FAB=$(getFabContract $CONTRACT_BIN/OperatorFab.bin "OPERATOR_FAB")
+OPERATOR_FAB=$(getFabContract src/lender/fabs/operator.sol:OperatorFab "OPERATOR_FAB")
 message "OPERATOR_FAB: $OPERATOR_FAB"
 
-COORDINATOR_FAB=$(getFabContract $CONTRACT_BIN/CoordinatorFab.bin "COORDINATOR_FAB")
+COORDINATOR_FAB=$(getFabContract src/lender/fabs/coordinator:CoordinatorFab "COORDINATOR_FAB")
 message "COORDINATOR_FAB: $COORDINATOR_FAB"
 
 # contract deployment
@@ -46,7 +47,7 @@ success_msg Lender Fabs ready
 
 ## backer allows lender to take currency
 message create lender deployer
-export LENDER_DEPLOYER=$(seth send --create $CONTRACT_BIN/LenderDeployer.bin 'LenderDeployer(address,address,address,address,address,address,address,address,address)' $ROOT_CONTRACT $TINLAKE_CURRENCY $TRANCHE_FAB $MEMBERLIST_FAB $RESTRICTEDTOKEN_FAB $RESERVE_FAB $ASSESSOR_FAB $COORDINATOR_FAB $OPERATOR_FAB)
+export LENDER_DEPLOYER=$(dapp create --verify "src/lender/deployer.sol:LenderDeployer" $ROOT_CONTRACT $TINLAKE_CURRENCY $TRANCHE_FAB $MEMBERLIST_FAB $RESTRICTEDTOKEN_FAB $RESERVE_FAB $ASSESSOR_FAB $COORDINATOR_FAB $OPERATOR_FAB)
 
 message "Init Lender Deployer"
 MIN_SENIOR_RATIO=$(seth --to-uint256 $MIN_SENIOR_RATIO)
@@ -58,13 +59,36 @@ seth send $LENDER_DEPLOYER 'init(uint,uint,uint,uint,uint, string memory,string 
 
 message deploy tranches
 seth send $LENDER_DEPLOYER 'deployJunior()'
+export JUNIOR_TRANCHE=$(seth call $LENDER_DEPLOYER 'juniorTranche()(address)')
+export JUNIOR_TOKEN=$(seth call $LENDER_DEPLOYER 'juniorToken()(address)')
+export JUNIOR_OPERATOR=$(seth call $LENDER_DEPLOYER 'juniorOperator()(address)')
+export JUNIOR_MEMBERLIST=$(seth call $LENDER_DEPLOYER 'juniorMemberlist()(address)')
+dapp verify-contract 'src/lender/token/restricted.sol' $JUNIOR_TOKEN '"$JUNIOR_TOKEN_SYMBOL"' '"$JUNIOR_TOKEN_NAME"'
+dapp verify-contract 'src/lender/tranche.sol' $JUNIOR_TRANCHE $TINLAKE_CURRENCY $JUNIOR_TOKEN
+dapp verify-contract 'src/lender/token/memberlist.sol' $JUNIOR_MEMBERLIST
+dapp verify-contract 'src/lender/operator.sol' $JUNIOR_OPERATOR $JUNIOR_TRANCHE
+
 seth send $LENDER_DEPLOYER 'deploySenior()'
+export SENIOR_TRANCHE=$(seth call $LENDER_DEPLOYER 'seniorTranche()(address)')
+export SENIOR_TOKEN=$(seth call $LENDER_DEPLOYER 'seniorToken()(address)')
+export SENIOR_OPERATOR=$(seth call $LENDER_DEPLOYER 'seniorOperator()(address)')
+export SENIOR_MEMBERLIST=$(seth call $LENDER_DEPLOYER 'seniorMemberlist()(address)')
+
 message deploy reserve
 seth send $LENDER_DEPLOYER 'deployReserve()'
+export RESERVE=$(seth call $LENDER_DEPLOYER 'reserve()(address)')
+dapp verify-contract 'src/lender/reserve.sol' $RESERVE $TINLAKE_CURRENCY
+
 message deploy assessor
 seth send $LENDER_DEPLOYER 'deployAssessor()'
+export ASSESSOR=$(seth call $LENDER_DEPLOYER 'assessor()(address)')
+dapp verify-contract 'src/lender/assessor.sol' $ASSESSOR
+
 message deploy coordinator
 seth send $LENDER_DEPLOYER 'deployCoordinator()'
+export COORDINATOR=$(seth call $LENDER_DEPLOYER 'coordinator()(address)')
+dapp verify-contract 'src/lender/coordinator.sol' $COORDINATOR $CHALLENGE_TIME
+
 message lender deployer rely/depend/file
 seth send $LENDER_DEPLOYER 'deploy()'
 
@@ -77,16 +101,16 @@ addValuesToFile $DEPLOYMENT_FILE <<EOF
     "TRANCHE_FAB"        :  "$TRANCHE_FAB",
     "MEMBERLIST_FAB"     :  "$MEMBERLIST_FAB",
     "RESERVE_FAB"        :  "$RESERVE_FAB",
-    "JUNIOR_OPERATOR"    :  "$(seth call $LENDER_DEPLOYER 'juniorOperator()(address)')",
-    "SENIOR_OPERATOR"    :  "$(seth call $LENDER_DEPLOYER 'seniorOperator()(address)')",
-    "JUNIOR_TRANCHE"     :  "$(seth call $LENDER_DEPLOYER 'juniorTranche()(address)')",
-    "SENIOR_TRANCHE"     :  "$(seth call $LENDER_DEPLOYER 'seniorTranche()(address)')",
-    "JUNIOR_TOKEN"       :  "$(seth call $LENDER_DEPLOYER 'juniorToken()(address)')",
-    "SENIOR_TOKEN"       :  "$(seth call $LENDER_DEPLOYER 'seniorToken()(address)')",
-    "JUNIOR_MEMBERLIST"  :  "$(seth call $LENDER_DEPLOYER 'juniorMemberlist()(address)')",
-    "SENIOR_MEMBERLIST"  :  "$(seth call $LENDER_DEPLOYER 'seniorMemberlist()(address)')",
-    "ASSESSOR"           :  "$(seth call $LENDER_DEPLOYER 'assessor()(address)')",
-    "COORDINATOR"        :  "$(seth call $LENDER_DEPLOYER 'coordinator()(address)')",
-    "RESERVE"            :  "$(seth call $LENDER_DEPLOYER 'reserve()(address)')"
+    "JUNIOR_OPERATOR"    :  "$JUNIOR_OPERATOR",
+    "SENIOR_OPERATOR"    :  "$SENIOR_OPERATOR",
+    "JUNIOR_TRANCHE"     :  "$JUNIOR_TRANCHE",
+    "SENIOR_TRANCHE"     :  "$SENIOR_TRANCHE",
+    "JUNIOR_TOKEN"       :  "$JUNIOR_TOKEN",
+    "SENIOR_TOKEN"       :  "$SENIOR_TOKEN",
+    "JUNIOR_MEMBERLIST"  :  "$JUNIOR_MEMBERLIST",
+    "SENIOR_MEMBERLIST"  :  "$SENIOR_MEMBERLIST",
+    "ASSESSOR"           :  "$ASSESSOR",
+    "COORDINATOR"        :  "$COORDINATOR",
+    "RESERVE"            :  "$RESERVE"
 }
 EOF
