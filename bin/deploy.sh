876def6c3c990167592a65d5537559717c26e707
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2021-01-22 14:13:11 +0100

    allow to pass config file as relative and absolute path

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,17 +1,18 @@
 #! /usr/bin/env bash
 
 set -e
+CONFIG_FILE=$1
+[ -z "$1" ] && CONFIG_FILE="./bin/config_$(seth chain).json"
+CONFIG_FILE=$(realpath "$CONFIG_FILE")
 
 BIN_DIR=${BIN_DIR:-$(cd "${0%/*}"&&pwd)}
 
 source $BIN_DIR/util/util.sh
 message Start Tinlake deployment
 
-cd $BIN_DIR
-CONFIG_FILE=$1
-[ -z "$1" ] && CONFIG_FILE="./config_$(seth chain).json"
-
 loadValuesFromFile $CONFIG_FILE
+
+cd $BIN_DIR
 
 # check if all required env variables are defined
 source $BIN_DIR/util/env-check.sh
